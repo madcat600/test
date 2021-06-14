0be45696d4476acc5ab387eca97d209127c4538e
Stas <madcat600@gmail.com>
2021-06-14 17:18:26 +0300

    commit

--- classData.js
+++ classData.js
@@ -2,16 +2,22 @@
 exports.__esModule = true;
 var classFight_1 = require("./classFight");
 var classSlave_1 = require("./classSlave");
-var armor1 = 50;
-var damage1 = 15;
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+var armor1 = getRandomInt(0, 100);
+var damage1 = getRandomInt(0, 50);
 var health1 = 100;
 var name1 = "Jim";
-var armor2 = 30;
-var damage2 = 7;
+var armor2 = getRandomInt(0, 100);
+var damage2 = getRandomInt(0, 50);
 var health2 = 100;
 var name2 = "Stan";
 var slave1 = new classSlave_1.Slave(armor1, damage1, health1, name1);
 var slave2 = new classSlave_1.Slave(armor2, damage2, health2, name2);
+//setTimeout(slave1.getGreeting, 500);
 slave1.getGreeting();
 slave2.getGreeting();
 new classFight_1.Fight(slave1, slave2).start();
--- classData.ts
+++ classData.ts
@@ -1,18 +1,25 @@
 import {Fight} from "./classFight"
 import {Slave} from "./classSlave"
 
-let armor1:number = 50;
-let damage1:number = 15;
+function getRandomInt(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+let armor1:number = getRandomInt(0, 100);
+let damage1:number = getRandomInt(0, 50);
 let health1:number = 100;
 let name1:string = "Jim";
 
-let armor2:number = 30;
-let damage2:number = 7;
+let armor2:number = getRandomInt(0, 100);
+let damage2:number = getRandomInt(0, 50);
 let health2:number = 100;
 let name2:string = "Stan";
 
 let slave1 = new Slave(armor1, damage1, health1, name1);
 let slave2 = new Slave(armor2, damage2, health2, name2);
+//setTimeout(slave1.getGreeting, 500);
 slave1.getGreeting();
 slave2.getGreeting();
 new Fight(slave1, slave2).start();
